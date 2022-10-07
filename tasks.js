e94dcf0116eabc7226fccdef32d84a35642ff303
natFrontDev <krupskaianatalia@gmail.com>
2022-10-07 19:25:33 +0300

    task #2

--- tasks.js
+++ tasks.js
@@ -2,3 +2,9 @@
 function reverseWords(str){
     return str.split(' ').reverse().join(' ')
 }
+
+// 2. Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+function abbrevName(name){
+    return name.split(' ').map(n => n[0].toUpperCase()).join('.')
+}
