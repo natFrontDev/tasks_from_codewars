c1e61f3e2cd99c83d4e8b9512110ccc34e8c6129
natFrontDev <krupskaianatalia@gmail.com>
2022-10-07 19:34:07 +0300

    task #5

--- tasks.js
+++ tasks.js
@@ -17,3 +17,8 @@ function digitize(n) {
 function makeUpperCase(str) {
     return str.toUpperCase()
 }
+
+// 5. Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+}
