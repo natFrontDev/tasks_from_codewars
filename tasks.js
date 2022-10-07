a3008015c9fbd492316b50d3f97487fff230a1c6
natFrontDev <krupskaianatalia@gmail.com>
2022-10-07 20:25:55 +0300

    task #14

--- tasks.js
+++ tasks.js
@@ -70,3 +70,21 @@ function persistence(num) {
     }
     return count
 }
+
+// 14. Equal Sides Of An Array
+function findEvenIndex(arr) {
+    let sum = 0,
+        leftSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    for (let i = 0; i < arr.length; i++) {
+        sum -= arr[i];
+        if (leftSum === sum) {
+            return i;
+        }
+        leftSum += arr[i];
+    }
+    return -1;
+}
+
