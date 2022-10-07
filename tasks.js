b9ec5f100f0bf3974dc384df08035f564d4fd8bc
natFrontDev <krupskaianatalia@gmail.com>
2022-10-07 19:20:40 +0300

    task #1

--- tasks.js
+++ tasks.js
@@ -1,1 +1,4 @@
-// first commit
+// 1. Complete the solution so that it reverses all of the words within the string passed in.
+function reverseWords(str){
+    return str.split(' ').reverse().join(' ')
+}
